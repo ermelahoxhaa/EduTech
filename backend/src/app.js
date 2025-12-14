c2c6fcb044a00e621b03f5fd1893ab2eea050c0b
eh700997 <ermelahoxha13@gmail.com>
2025-12-14 21:48:39 +0100

    error fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,29 @@
 import express from "express";
 import cors from "cors";
 
+import notificationRoutes from "./routes/notifications.js";
+import coursesRoutes from "./routes/courses.js";
+import usersRoutes from "./routes/users.js";
+
 const app = express();
 
-import notificationRoutes from './routes/notifications.js';
-import coursesRoutes from './routes/courses.js';
-import usersRoutes from './routes/users.js';
+app.use(cors({
+  origin: "http://localhost:8080",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.use('/api/courses', coursesRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/notifications', notificationRoutes);
 
-app.use(cors());
 app.use(express.json());
+
+app.use("/api/courses", coursesRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is ready!");
 });
-
 
 const PORT = 5000;
 app.listen(PORT, () => {
